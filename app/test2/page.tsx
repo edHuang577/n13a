446352fdd06c12dda16b2366315bdbbc899e3409
edHuang577 <112370856+edHuang577@users.noreply.github.com>
2022-12-22 15:52:32 +0800

    err

---
+++ app/test2/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    const fetchText = async () => {
+      // 這裡是抓取 web service 的程式碼
+      const response = await fetch("/api/hello");
+      const data = await response.text();
+      setText(data);
+    };
+
+    const interval = setInterval(fetchText, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default App;
